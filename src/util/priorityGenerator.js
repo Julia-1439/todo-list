ea04f3542e2a8b939dc4921e1f6a901152a5d02b
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-17 16:22:26 -0700

    Create utility modules

---
+++ src/util/priorityGenerator.js
@@ -0,0 +1,20 @@
+export function createLowPrio() {
+  return {
+    text: "low",
+    color: "#FFF57D",
+  };
+}
+
+export function createMedPrio() {
+  return {
+    text: "medium",
+    color: "#FFA257",
+  };
+}
+
+export function createHighPrio() { 
+  return {
+    text: "high",
+    color: "#FF5757",
+  };
+}
---
+++ src/util/statusGenerator.js
@@ -0,0 +1,13 @@
+export function createIncompleteStatus() {
+  return {
+    text: "Incomplete",
+    color: "#9b9b9b",
+  };
+}
+
+export function createCompletedStatus() {
+  return {
+    text: "Completed",
+    color: "#80ff80ff",
+  };
+}
