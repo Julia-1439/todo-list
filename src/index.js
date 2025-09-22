6bf6f1f5718b605d5aa59fc6cf33103641b9b122
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-22 16:00:06 -0700

    Move tests to its own file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,3 @@
-/* ========================================================================== */
-/* TEST INPUTS */
-/* ========================================================================== */
+import { runTests } from "./tester.js";
 
-import { Project } from "./barrel.js";
-import { TodoItem } from "./barrel.js";
-import { ChecklistItem } from "./barrel.js";
-
-import { storageController } from "./barrel.js";
-import { internalController } from "./barrel.js";
-
-const todo = new TodoItem({
-  title: "Pick up kid from school",
-  dueDate: "today",
-  status: "incomplete",
-  priority: "high",
-});
-todo.addChecklistItem(new ChecklistItem({ description: "get in the car"})); 
-todo.addChecklistItem(new ChecklistItem({ description: "drive to school"})); 
-todo.deleteChecklistItemAt(1);
-console.log(todo);
-
-const project = new Project({title: "Meow at 10 decibels"});
-console.log(project);
-
+runTests();
