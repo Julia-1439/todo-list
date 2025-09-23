b23ba4e3fb4f94f2cdf87a3bbb343a73c5d050af
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-23 09:55:46 -0700

    Adjust indendentation & scope for tests

--- src/tester.js
+++ src/tester.js
@@ -9,49 +9,47 @@ import { storageController } from "./barrel.js";
 import { internalController } from "./barrel.js";
 
 function runTests() {
-  (() => {
-    const todo = new TodoItem({
-      title: "Buy groceries",
-      description: "Go to the Shop. Remember to bring a bag to avoid paying the fee",
-      dueDate: new Date(2025, 9-1, 2),
-      priority: new priorityGenerator.createPrio1(),
-    })
-    console.log(todo);
-    console.log(todo.status);
-    console.log(todo.priority);
+  const todo = new TodoItem({
+    title: "Buy groceries",
+    description: "Go to the Shop. Remember to bring a bag to avoid paying the fee",
+    dueDate: new Date(2025, 9-1, 2),
+    priority: new priorityGenerator.createPrio1(),
+  })
+  console.log(todo);
+  console.log(todo.status);
+  console.log(todo.priority);
 
-    todo.edit({
-      title: "Buy hardware",
-      status: new statusGenerator.createCompletedStatus(),
-      priority: new priorityGenerator.createPrio2(),
-    });
-    console.log(todo);
-    console.log(todo.status);
-    console.log(todo.priority);
+  todo.edit({
+    title: "Buy hardware",
+    status: new statusGenerator.createCompletedStatus(),
+    priority: new priorityGenerator.createPrio2(),
+  });
+  console.log(todo);
+  console.log(todo.status);
+  console.log(todo.priority);
 
 
-    const project = new Project({
-      title: "Cook a goulash",
-      description: "This is my first attempt at cooking an American goulash! I'm so excited."
-    });
-    console.log(project);
-    console.log(project.status);
+  const project = new Project({
+    title: "Cook a goulash",
+    description: "This is my first attempt at cooking an American goulash! I'm so excited."
+  });
+  console.log(project);
+  console.log(project.status);
 
-    project.editMetadata({
-      title: "Cook a delicious goulash",
-      status: new statusGenerator.createCompletedStatus(),
-    });
-    console.log(project);
-    console.log(project.status);
+  project.editMetadata({
+    title: "Cook a delicious goulash",
+    status: new statusGenerator.createCompletedStatus(),
+  });
+  console.log(project);
+  console.log(project.status);
 
-    project.addTodo(todo);
-    project.addTodo(new TodoItem({title: "test todo2"}));
-    project.addTodo(new TodoItem({title: "test todo3"}));
-    console.log(project.todoList);
+  project.addTodo(todo);
+  project.addTodo(new TodoItem({title: "test todo2"}));
+  project.addTodo(new TodoItem({title: "test todo3"}));
+  console.log(project.todoList);
 
-    project.removeTodo(todo.uuid);
-    console.log(project.todoList);
-  })();
+  project.removeTodo(todo.uuid);
+  console.log(project.todoList);
 }
 
 export { runTests };
