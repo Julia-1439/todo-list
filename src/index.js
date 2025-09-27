5e7a1648d16e086793e7ffa76f7980a60c026306
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-09-26 18:09:26 -0700

    Move tests to tester.js

--- src/index.js
+++ src/index.js
@@ -5,40 +5,5 @@ import {
   runTestSet4,
   createProjectsHelper,
   runTestSet5, 
+  runTestSet6,
 } from "./tester.js";
-
-import { internalControl as ic } from "./barrel.js";
-
-const loadData = ic.initialize();
-
-let homeProjectData = loadData.latestProject; 
-const homeProjUuid = homeProjectData.uuid;
-homeProjectData = ic.editProjectMetadata(homeProjUuid, {
-  status: "completed",
-});
-
-let todo1Data = loadData.latestProject.todoList[0];
-const todo1Uuid = todo1Data.uuid;
-todo1Data = ic.editTodo(homeProjUuid, todo1Uuid,
-  {
-    status: "completed",
-    priority: "p1",
-  }
-);
-
-let todo2Data = loadData.latestProject.todoList[1];
-const todo2Uuid = todo2Data.uuid;
-todo2Data = ic.editTodo(homeProjUuid, todo2Uuid,
-  {
-    priority: null,
-  }
-);
-
-console.log(
-  "Home project data:",
-  homeProjectData,
-  "Todo1 data:",
-  todo1Data,
-  "Todo2 data:",
-  todo2Data,
-)
--- src/tester.js
+++ src/tester.js
@@ -358,6 +358,45 @@ function runTestSet5() {
   );
 }
 
+// internalControl: return values & no localStorage test
+function runTestSet6() {
+  const ic = internalControl;
+
+  const loadData = ic.initialize();
+
+  let homeProjectData = loadData.latestProject; 
+  const homeProjUuid = homeProjectData.uuid;
+  homeProjectData = ic.editProjectMetadata(homeProjUuid, {
+    status: "completed",
+  });
+
+  let todo1Data = loadData.latestProject.todoList[0];
+  const todo1Uuid = todo1Data.uuid;
+  todo1Data = ic.editTodo(homeProjUuid, todo1Uuid,
+    {
+      status: "completed",
+      priority: "p1",
+    }
+  );
+
+  let todo2Data = loadData.latestProject.todoList[1];
+  const todo2Uuid = todo2Data.uuid;
+  todo2Data = ic.editTodo(homeProjUuid, todo2Uuid,
+    {
+      priority: null,
+    }
+  );
+
+  console.log(
+    "Home project data:",
+    homeProjectData,
+    "Todo1 data:",
+    todo1Data,
+    "Todo2 data:",
+    todo2Data,
+  );
+}
+
 export { 
   runTestSet1, 
   runTestSet2, 
@@ -365,4 +404,5 @@ export {
   runTestSet4,
   createProjectsHelper,
   runTestSet5,
+  runTestSet6,
 };
