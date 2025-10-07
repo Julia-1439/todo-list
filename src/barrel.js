ecba4f0ed7ab2ea961845df898e5a056e4c8c377
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-10-07 14:02:38 -0700

    Refactor: organize barrel module

--- src/barrel.js
+++ src/barrel.js
@@ -1,20 +1,11 @@
-// Util for classes
-export * as uuidGenerator from "./classes/util/uuidGenerator.js";
-export * as statusUtil from "./classes/util/statusUtil.js";
-export * as priorityUtil from "./classes/util/priorityUtil.js";
-
-// Classes
-export { default as Project } from "./classes/project.js";
-export { default as TodoItem } from "./classes/todoItem.js";
-
 // Controllers
-export * as storageControl from "./storageControl.js";
-export * as internalControl from "./internalControl.js";
-export * as displayControl from "./displayControl.js";
 export * as app from "./app.js";
+export * as displayControl from "./displayControl.js";
+export * as internalControl from "./internalControl.js";
+export * as storageControl from "./storageControl.js";
 
-// Styling
-export * as style from "./style.css";
+// Util (general usage)
+export * as dateFns from "./util/dateFns.js";
 
 // Content rendering
 export * as sidebarRenderer from "./content-rendering/sidebar.js";
@@ -23,8 +14,17 @@ export * as projectSelectElementRenderer from "./content-rendering/projectSelect
 export * as contextMenusRenderer from "./content-rendering/contextMenus.js";
 export * as notifRenderer from "./content-rendering/notif.js";
 
-// Util for content rendering
+// Util (for content rendering)
 export * as svgCreator from "./content-rendering/util/svgCreator.js";
 
-// Util general
-export * as dateFns from "./util/dateFns.js";
+// Classes
+export { default as Project } from "./classes/project.js";
+export { default as TodoItem } from "./classes/todoItem.js";
+
+// Util (for classes)
+export * as uuidGenerator from "./classes/util/uuidGenerator.js";
+export * as statusUtil from "./classes/util/statusUtil.js";
+export * as priorityUtil from "./classes/util/priorityUtil.js";
+
+// Styling
+export * as style from "./style.css";
